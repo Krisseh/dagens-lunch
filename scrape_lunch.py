21b40ad5ccfb6f273e457043eb71a5f21495b044
Krisseh <krissethenisse@gmail.com>
2025-12-17 15:50:07 +0100

    Update scrape_lunch.py

--- scrape_lunch.py
+++ scrape_lunch.py
@@ -149,8 +149,8 @@ def crop_day_from_image(image, day):
             )
 
             # 🔧 TIGHTARE HORISONTELL CROP
-            left = max(0, x - 10)
-            right = min(image.width, x + w + 368)
+            left = max(0, x - 20)
+            right = min(image.width, x + w + 308)
 
             return image.crop((left, top, right, bottom))
 
